d42d33cf7dbacc97ec497e7fcca578860aa8881f
Ali Rafay <ali.rafay@uniquesoftwaredev.com>
2024-03-11 20:58:25 +0500

    implementation of OOP in project

---
+++ DiawiService.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from SkypeService import SkypeService
+import requests
+import shutil
+import traceback
+import os
+from Constants import Settings, SeleniumXpaths, SystemPaths, WebsiteLink, LoadingState
+from Credentials import Credentials
+ 
+
+
+class DiawiService():
+    driver = None
+    def __init__(self) -> None:
+        chrome_options = Options()
+        chrome_options.add_argument(Settings.HEADLESSMODE)
+        chrome_options.add_argument(Settings.LOG_LEVEL3)
+        chrome_options.add_argument(Settings.IGNORE_CERTIFICATE_ERRORS)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    def UploadToDiawi(self, full_name, app_path=None, message='', skypeService:SkypeService=None):
+        self.driver.get(WebsiteLink.Diawi)
+
+        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, SeleniumXpaths.LoginPageButton)))
+        self.driver.find_element(By.XPATH, SeleniumXpaths.LoginPageButton).click()
+
+        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, SeleniumXpaths.EmailAddress)))
+        self.driver.find_element(By.XPATH, SeleniumXpaths.EmailAddress).send_keys(Credentials.DiawiEmail)
+
+        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, SeleniumXpaths.Password)))
+        self.driver.find_element(By.XPATH, SeleniumXpaths.Password).send_keys(Credentials.DiawiPassword)
+
+        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, SeleniumXpaths.LoginButton)))
+        self.driver.find_element(By.XPATH, SeleniumXpaths.LoginButton).click()
+
+        self.driver.get(WebsiteLink.Diawi)
+
+        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, SeleniumXpaths.FileUploadXpath)))
+        self.driver.find_element(By.XPATH, SeleniumXpaths.FileUploadXpath).send_keys(app_path)
+
+        #waiting for loading
+        while self.driver.find_element(By.XPATH, SeleniumXpaths.LoadingText).text != LoadingState.Completed:
+            continue
+
+        self.driver.find_element(By.XPATH, SeleniumXpaths.SubmitButtonXpath).click()
+
+        WebDriverWait(driver=self.driver, timeout=500).until(EC.presence_of_element_located(((By.XPATH, SeleniumXpaths.UrlLinkElement))))
+        link = self.driver.find_element(By.XPATH, SeleniumXpaths.UrlLinkElement).text
+        image = self.driver.find_element(By.XPATH, SeleniumXpaths.ImageElementXpath)
+
+        src = ''
+        while src == '':
+            src = image.get_attribute('src')
+            url = src
+
+        response = requests.get(url, stream=True)
+
+        with open(SystemPaths.ImageFolderPath, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+
+        try:
+            skypeService.SendMsgToSkype(full_name, SystemPaths.ImageFolderPath, f"{message} Diawi Link: {link}", image=True)
+            os.remove(SystemPaths.ImageFolderPath)
+
+        except:
+            print("An exception occurred")
+            traceback.print_exc()
--- y.py
+++ y.py
@@ -2,51 +2,64 @@
 import os
 import subprocess
 import sys
-from SkypeService import SkypeService
-from DiawiService import DiawiService
-from Constants import OS, PathConstants
-from reusableFunctions import jarvis_init
+import SkypeService
+import DiawiService
 
-
-
-def jarvis_build(directory_path, build_type, sendToWhom, skypeService:SkypeService, diawiService:DiawiService): 
+def jarvis_build(directory_path, build_type, sendToWhom): 
     try:
-        full_path:str = jarvis_init(directory_path)
+        
+        base_path = r"C:\AashirRaza\Koderlabs"
+        full_path = os.path.join(base_path, directory_path)
+
+        print("-------> Navigating to directory")
+
+        os.chdir(full_path)
+
+        print("-------> Directory navigation successful.\n")
+            
 
         if build_type == "release":
+            print("-------> Making release build\n")
+
             # Change to the "android" directory
             android_path = os.path.join(full_path, "android")
             os.chdir(android_path)
-            subprocess.run(["./gradlew assembleRelease"] if OS.IOS else ["gradlew", "assembleRelease"], shell=True, check=True)
+
+            subprocess.run(["gradlew", "assembleRelease"], shell=True, check=True)
 
             # Determine the package size
-            package_path = os.path.join(android_path, PathConstants.AndroidPathAPK)
+            package_path = os.path.join(
+                android_path,
+                r"app\build\outputs\apk\release\app-release.apk",
+            )
+
+
             package_size = os.path.getsize(package_path) / (1024 * 1024)
 
-            if package_size < 250:
+            print(f"Package size is {package_size} MB")
+
+            if package_size < 60:
                 print("Uploading build to Diawi...")
-                diawiService.UploadToDiawi(sendToWhom, package_path, directory_path, skypeService)
+
+                DiawiService.UploadToDiawi(sendToWhom, package_path, directory_path)
+                # sendToSkype.SendMsgToSkype(sendToWhom , package_path, "{} build".format(directory_path))
+                
+                # Place your Diawi upload logic here
+                # You might use requests to upload the build to Diawi
+
             else:
-                skypeService.SendMsgToSkype(sendToWhom , package_path, "{} build".format(directory_path))
+                SkypeService.SendMsgToSkype(sendToWhom , package_path, "{} build".format(directory_path))
+                print(f"Build package is too large to upload to Diawi ({package_size} MB).")
 
         if build_type == "bundle":
             print("Making bundle build...")
 
-            android_path = os.path.join(full_path, "android")
-            os.chdir(android_path)
-
-            subprocess.run(["./gradlew bundleRelease"] if OS.IOS else ["gradlew", "bundleRelease"], shell=True, check=True)
-            package_path = os.path.join(android_path, PathConstants.AndroidPathBundle)
-            skypeService.SendMsgToSkype(sendToWhom , package_path, "{} build".format(directory_path))
     except:
         pass
 
 
 
 if __name__ == "__main__":
-    skypeService = SkypeService()
-    diawiService = DiawiService()
-
     print("\n")
     print("""
                      .B@@@@@@!  ?###&&&@@@@   . ^5GB#&&&@@@@@@&B7 !@@@@@@7  J@@@@@&:JGGB#&G    :!YPB&&@@@&#G 
@@ -69,8 +82,9 @@ if __name__ == "__main__":
 
     directory_path = sys.argv[1]
     build_type = sys.argv[2]
-    sendToWhom = sys.argv[3:]
+    sendToWhom = sys.argv[3]
 
-    jarvis_build(directory_path, build_type, sendToWhom, skypeService, diawiService)
+    jarvis_build(directory_path, build_type, sendToWhom)
+
 
 
