2fe329eb37bb7c528d9bb666c78f94b3cee83600
Syed Aashir Raza <syed.aashir@koderlabs.com>
2024-03-12 15:42:14 +0500

    remove unused files

--- Constants.py
+++ Constants.py
@@ -21,20 +21,16 @@ class SeleniumXpaths:
     UrlLinkElement = r"/html/body/section/section[2]/div[1]/div/div[1]/div[2]/a"
     ImageElementXpath = r'//*[@id="main-container"]/section[2]/div[1]/div/div[2]/img'
 
-
 class Settings:
     HEADLESSMODE = "--headless"
     LOG_LEVEL3 = "--log-level=3"
     IGNORE_CERTIFICATE_ERRORS = "--ignore-certificate-errors"
 
-
 class SystemPaths:
     ImageFolderPath = r"/Users/aashirraza/Desktop/Tests/Jarvis/temp/app_qrcode.png"
 
-
 class WebsiteLink:
     Diawi = "https://www.diawi.com/"
-
 
 class LoadingState:
     Completed = "100%"
